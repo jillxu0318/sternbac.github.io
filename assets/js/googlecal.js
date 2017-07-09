b5ad210f453d5dbf940d3edfd2287f014c518d9f
jillxu0318 <jillxu10@gmail.com>
2017-07-08 22:23:32 -0400

    switched to analytic@stern.nyu.edu account and added logic to just extract the next three events

--- assets/js/googlecal.js
+++ assets/js/googlecal.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
     var GOOGLE_CAL_URL = 'https://www.googleapis.com/calendar/v3/calendars/';
-    var API_KEY = 'AIzaSyCWLHoex4mBTrqBtRN9T9p6o9jK0n50noI';
-    var CALENDAR_ID = 'ehjp2rfjn42fanc8t7ep27llts@group.calendar.google.com';
-    $.get(GOOGLE_CAL_URL + CALENDAR_ID + '/events?key=' + API_KEY, function(data) {
+    var API_KEY = 'AIzaSyABek6rqw9ZTqA9vZLJ84YTA1YG0cgDMWE';
+    var CALENDAR_ID = 'analytic@stern.nyu.edu';
+
+    var params = { 
+        timeMin: (new Date()).toISOString(),
+        showDeleted: false,
+        singleEvents: true,
+        maxResults: 3,
+        orderBy: 'startTime',
+        key: API_KEY
+    };
+
+    console.log($.param(params));
+
+    $.get(GOOGLE_CAL_URL + CALENDAR_ID + '/events?' + $.param(params), function(data) {
         for (var i = 1; i <= 3; i++) {
             $("#eventTitle" + i).text(data.items[i - 1].summary);
             $("#eventDetails" + i).text(data.items[i - 1].description);

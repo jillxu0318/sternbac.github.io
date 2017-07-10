5e4d58adafb5ffa19aef71f290a982646e5fb29d
jillxu0318 <jillxu10@gmail.com>
2017-07-09 23:55:43 -0400

    created the dropdown menu and hooked with google calendar

--- assets/js/googlecal.js
+++ assets/js/googlecal.js
@@ -7,17 +7,43 @@ $(document).ready(function() {
         timeMin: (new Date()).toISOString(),
         showDeleted: false,
         singleEvents: true,
-        maxResults: 3,
         orderBy: 'startTime',
         key: API_KEY
     };
 
     // https://www.googleapis.com/calendar/v3/calendars/analytic@stern.nyu.edu/events?key=AIzaSyABek6rqw9ZTqA9vZLJ84YTA1YG0cgDMWE
 
-    $.get(GOOGLE_CAL_URL + CALENDAR_ID + '/events?' + $.param(params), function(data) {
-        for (var i = 1; i <= 3; i++) {
-            $("#eventTitle" + i).text(data.items[i - 1].summary);
-            $("#eventDetails" + i).text(data.items[i - 1].description);
-        }
-    });
+    // if loading page is index.html
+    if (top.location.pathname.indexOf('index.html') !== -1) {
+        populate_homepage(3);
+    } else if (top.location.pathname.indexOf('calendar.html') !== -1) { 
+        // if loading page is calendar.html
+        populate_calendar_page();
+
+    }
+
+    function populate_homepage(count) {
+        params.maxResults = count;
+        $.get(GOOGLE_CAL_URL + CALENDAR_ID + '/events?' + $.param(params), function(data) {
+            for (var i = 1; i <= 3; i++) {
+                $("#eventTitle" + i).text(data.items[i - 1].summary);
+                $("#eventDetails" + i).text(data.items[i - 1].description);
+            }
+        });           
+    }
+
+    function populate_calendar_page() {
+        $.get(GOOGLE_CAL_URL + CALENDAR_ID + '/events?' + $.param(params), function(data) {
+            console.log(data);
+            $ul = $("#club_events");
+            $.each(data.items, function(index, value) {
+                var link = value.htmlLink;
+                var name = value.summary;
+                var li = $("<li></li>");
+                var a = $("<a></a>").attr("href", link).text(name);
+
+                $ul.append(li.append(a));
+            });
+        });           
+    }
 });

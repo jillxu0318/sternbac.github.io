f9b9a35874c1afe8f4d83321ccea73160d430ada
Jill Xu <jillxu10@gmail.com>
2017-09-04 21:45:02 -0400

    fixed bachallenge page

--- assets/js/doctree.js
+++ assets/js/doctree.js
@@ -6,15 +6,15 @@ var docTree = [
         },
         nodes: [
             {
-                text: "BaChallenge_-Gerrymandering.pdf",
+                text: "Gerrymandering",
                 href: "https://drive.google.com/open?id=0ByJYMvzdQFKAa1pfTnN5RGw1R1dtTURPaEFQZ2ROQ0gyZDZJ"
             },
             {
-                text: "NBA Analytics Presentation.pdf",
+                text: "NBA Analytics",
                 href: "https://drive.google.com/open?id=0ByJYMvzdQFKAQmp0X3Jqb1hHeUR4RGJaaFh5ZHZFQmpiU04w"
             },
             {
-                text: "Lending Club Presentation FINAL.pdf",
+                text: "Lending Club",
                 href: "https://drive.google.com/open?id=0ByJYMvzdQFKAVmIyazlteDNZeEU"
             }
         ]
